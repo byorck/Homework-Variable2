5e74b42c22df9b532def242787acf212e52c7260
byorck <work-byorck@yandex.ru>
2024-12-23 16:10:48 +0300

    HW complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int four = 2147483647;  //задача № 1
+        byte one = 127;
+        short two = 32766;
+        long eight = 9223372036854775807L;
+        float pointFour = 3.4f;
+        double pointEight = 1.7;
+        System.out.println("Значение переменной с типом " + "int" + " равно " + four);
+        System.out.println("Значение переменной с типом " + "byte" + " равно " + one);
+        System.out.println("Значение переменной с типом " + "short" + " равно " + two);
+        System.out.println("Значение переменной с типом " + "long" + " равно " + eight);
+        System.out.println("Значение переменной с типом " + "float" + " равно " + pointFour);
+        System.out.println("Значение переменной с типом " + "double" + " равно " + pointEight);
+        double first = 27.12;  //задача № 2
+        long second = 987678965549L;
+        double third = 2.786;
+        short fourth = 569;
+        short five = -159;
+        double six = 27897;
+        int seven = 67;
+        byte firstTeacher = 23;  //задача № 3
+        byte secondTeacher = 27;
+        byte thirdTeacher = 30;
+        short summarySheets = 420;
+        int sheetsPerOneStudent = summarySheets / (firstTeacher + secondTeacher + thirdTeacher);
+        System.out.println("На каждого ученика расчитано " + sheetsPerOneStudent + " листов бумаги");
+        byte bottlePerMinute = 16 / 2;  //задача № 4
+        int bottlePerDay = bottlePerMinute * 1440;
+        System.out.println("За " + "20 минут" + " машина произвела " + (bottlePerMinute * 20) + " штук бутылок");
+        System.out.println("За " + "сутки" + " машина произвела " + bottlePerDay + " штук бутылок");
+        System.out.println("За " + "3 дня" + " машина произвела " + (bottlePerDay * 3) + " штук бутылок");
+        System.out.println("За " + "месяц" + " машина произвела " + (bottlePerDay * 365) + " штук бутылок");
+        byte countClassroom = 120 / (2 + 4);  //задача № 5
+        int countWhitePaint = countClassroom * 2;
+        int countBrownPaint = countClassroom * 4;
+        System.out.println("В школе, где " + countClassroom + " классов, нужно " + countWhitePaint + " банок белой краски и " + countBrownPaint + " банок коричневой краски");
+        byte banana = 80;  //задача № 6
+        byte milk = 105;
+        byte iceCream = 100;
+        byte egg = 70;
+        int summaryWeightInGr = banana * 5 + milk * 2 + iceCream * 2 + egg * 4;
+        float summaryWeightInKg = summaryWeightInGr / 1000f;
+        System.out.println(summaryWeightInGr);
+        System.out.println(summaryWeightInKg);
+        byte necessaryWeightInKg = 7;  //задача № 7
+        int necessaryWeightInGr = necessaryWeightInKg * 1000;
+        int countDay250 = necessaryWeightInGr / 250;
+        int countDay500 = necessaryWeightInGr / 500;
+        int averageDay = (countDay250 + countDay500) / 2;
+        System.out.println(countDay250);
+        System.out.println(countDay500);
+        System.out.println(averageDay);
+        int wagesMasha = 67760;  //задача № 8
+        int wagesDenis = 83690;
+        int wagesKristin = 76230;
+        int increasedWagesMasha = wagesMasha * 110 / 100;
+        int increasedWagesDenis = wagesDenis * 110 / 100;
+        int increasedWagesKristin = wagesKristin * 110 / 100;
+        int differenceWagesMasha = increasedWagesMasha - wagesMasha;
+        int differenceWagesDenis = increasedWagesDenis - wagesDenis;
+        int differenceWagesKristin = increasedWagesKristin - wagesKristin;
+        System.out.println("Маша теперь получает " + increasedWagesMasha + " рублей. Годовой доход вырос на " + differenceWagesMasha + " рублей");
+        System.out.println("Денис теперь получает " + increasedWagesDenis + " рублей. Годовой доход вырос на " + differenceWagesDenis + " рублей");
+        System.out.println("Кристина теперь получает " + increasedWagesKristin + " рублей. Годовой доход вырос на " + differenceWagesKristin + " рублей");
     }
 }
